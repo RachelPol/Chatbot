cb826a6a9e48dce8c02ae13b8f6f0eeaa6c8b6a0
פנינה ברנדייס <p0548509145@gmail.com>
2022-02-21 23:28:53 +0200

    Bag num 3

--- Plugins/List/ListPlugin.cs
+++ Plugins/List/ListPlugin.cs
@@ -23,34 +23,35 @@ namespace ListPlugin
             {
                 list = JsonSerializer.Deserialize<PersistentDataStructure>(input.PersistentData).List;
             }
-
+            string s = input.Message.ToString().ToLower();
             if (input.Message == "")
             {
                 input.Callbacks.StartSession();
                 return new PluginOutput("List started. Enter 'Add' to add task. Enter 'Delete' to delete task. Enter 'List' to view all list. Enter 'Exit' to stop.", input.PersistentData);
             }
-            else if (input.Message == "exit")
+            else if (s == "exit")
             {
                 input.Callbacks.EndSession();
                 return new PluginOutput("List stopped.", input.PersistentData);
             }
-            else if (input.Message.StartsWith("add"))
+           
+            else if (s.StartsWith("add"))
             {
-                var str = input.Message.Substring("add".Length).Trim();
+                var str = s.Substring("add".Length).Trim();
                 list.Add(str);
 
                 var data = new PersistentDataStructure(list);
 
                 return new PluginOutput($"New task: {str}", JsonSerializer.Serialize(data));
             }
-            else if (input.Message.StartsWith("delete"))
+            else if (s.StartsWith("delete"))
             {   
                 list.RemoveAt(list.Count - 1);
                 var data = new PersistentDataStructure(list);
 
                 return new PluginOutput($"Delete last task");
             }
-            else if (input.Message == "list")
+            else if (s == "list")
             {
                 string listtasks = string.Join("\r\n", list);
                 return new PluginOutput($"All list tasks:\r\n{listtasks}", input.PersistentData);
