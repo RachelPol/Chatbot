0b11de704447f2d4ea92771ba16b91f3919ed218
ayalaMualem <a0548574212@gmail.com>
2025-01-15 11:21:26 +0200

    homwork changes

--- Plugins/Counter/CounterPlugin.cs
+++ Plugins/Counter/CounterPlugin.cs
@@ -11,7 +11,8 @@ namespace Counter
 
         public PluginOutput Execute(PluginInput input)
         {
-            var lastCount = int.Parse(input.PersistentData);
+            
+            var lastCount = int.Parse(input.PersistentData??"0");
             var result = (lastCount + 1).ToString();
             return new PluginOutput(result, result);
         }
