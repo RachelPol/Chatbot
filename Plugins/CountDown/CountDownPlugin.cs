d363e35a21a6455f2629ad3c16b2ad49b964f5ec
efratkitov <ekt82577@gmail.com>
2025-01-19 21:20:56 +0200

    תיקון יכולת countdown

--- Plugins/CountDown/CountDownPlugin.cs
+++ Plugins/CountDown/CountDownPlugin.cs
@@ -15,29 +15,12 @@ namespace CountDown
         public static string _Id = "count-down";
         public string Id => _Id;
 
-        //public PluginOutput Execute(PluginInput input)
-        //{
-        //    var interval = int.Parse(input.Message);
-        //    _scheduler.Schedule(TimeSpan.FromSeconds(interval), Id, "");
-        //    return new PluginOutput("Countdown started.");
-        //
-        //}
         public PluginOutput Execute(PluginInput input)
-        {
-            Console.WriteLine($"Plugin {Id} is executing with input: {input.Message}");
-
-            var cleanedInput = input.Message.Trim();
-            Console.WriteLine($"Received input after cleaning: {cleanedInput}");
-
-            if (!int.TryParse(cleanedInput, out int interval))
-            {
-                return new PluginOutput("Invalid input. Please provide a valid number of seconds.");
-            }
-
-            _scheduler.Schedule(TimeSpan.FromSeconds(interval), Id, "");
-            return new PluginOutput("Countdown started.");
+        {   int interval=0;
+            var isSucces = int.TryParse(input.Message,out interval);
+            _scheduler.Schedule(TimeSpan.FromSeconds(interval),Id,"");
+           return new PluginOutput("Countdown started.");
         }
-
         public void OnScheduler(string data)
         {
             Console.WriteLine("Fired.");
